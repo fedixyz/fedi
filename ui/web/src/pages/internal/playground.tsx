68b2feed5ea49ca00137de80493122553be69ec3
Fedi CI <ci@fedi.xyz>
2026-02-13 09:38:03 +0000

    chore: remove playground code (#10247)
    source commit: 7ce80d003ccc2e9544d834e45bea3bd008fcff3e

--- ui/web/src/pages/internal/playground.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { ComponentPlayground } from '../../components/ComponentPlayground'
-
-const Playground: React.FC = () => {
-    return <ComponentPlayground />
-}
-
-export default Playground
